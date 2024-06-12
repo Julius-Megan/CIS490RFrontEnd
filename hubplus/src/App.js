ad14d1f2d7c0628edddff6b0191e1615c89e4cf2
Julius Such <juliussuch@gmail.com>
2024-06-12 04:42:19 -1000

    Add Footer

--- hubplus/src/App.js
+++ hubplus/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <HeroSection/>
+            <Footer/>
             {/* Other components go here */}
         </div>
     );
---
+++ hubplus/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2024 Your Company. All rights reserved.</p>
+                <ul className="footer-links">
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#privacy">Privacy Policy</a></li>
+                </ul>
+                <div className="footer-social">
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
