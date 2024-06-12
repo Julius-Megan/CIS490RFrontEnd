a06c599a42436497c9a52b8714f674746c6972c1
Julius Such <juliussuch@gmail.com>
2024-06-12 04:21:17 -1000

    Change button color

--- hubplus/src/components/HeroSection.js
+++ hubplus/src/components/HeroSection.js
@@ -1,7 +1,6 @@
 // HeroSection.js
 import React from 'react';
 import './HeroSection.css'; // Import CSS for styling
-import backgroundImage from './hubDoor.jpg';
 
 const HeroSection = () => {
     return (
@@ -9,7 +8,7 @@ const HeroSection = () => {
             <div className="hero-content">
                 <h1 className="hero-title">Welcome to Our Website</h1>
                 <p className="hero-subtitle">We provide the best services to help you succeed.</p>
-                <a href="#services" className="hero-button">Learn More</a>
+                <a href="#services" className="hero-button">Enter HUB</a>
             </div>
         </section>
     );
