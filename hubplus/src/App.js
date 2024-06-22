78f7ed8a0e18d32370cd5c457749621960a1ae96
Julius Such <juliussuch@gmail.com>
2024-06-21 22:39:07 -1000

    Create a LoginForm

--- hubplus/src/App.js
+++ hubplus/src/App.js
@@ -3,15 +3,20 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
+import LoginForm from './components/LoginForm/LoginForm';
 
 const App = () => {
     return (
         <div>
+            <LoginForm/>
+            {/* 
+            
             <Navbar />
             <HeroSection/>
             
             <Footer/>
-            {/* Other components go here */}
+            
+            Other components go here */}
         </div>
     );
 }
